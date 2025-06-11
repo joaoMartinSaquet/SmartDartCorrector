5afc4ac57d4c719c16c3837b21783ef80f3bfa2d
MOI <jmartinsaquet@lrtechnologies.fr>
2025-06-11 10:36:29 +0200

    common creation

---
+++ common/__init__.py
@@ -0,0 +1,15 @@
+"""SmartDartCorrector Common Package
+
+This package contains common utilities and shared components for the SmartDartCorrector project.
+"""
+
+__version__ = "1.0.0"
+__author__ = "SmartDartCorrector Team"
+
+# Import main modules for easier access
+from .rolloutenv import *
+
+__all__ = [
+    "rolloutenv",
+]
+
