40e74438f0d3eda037ea846cf9e19a65fbb7dee0
MOI <jmartinsaquet@lrtechnologies.fr>
2025-06-11 10:41:27 +0200

    rename to meaningfull scripts

---
+++ run_corrector.py
@@ -0,0 +1,29 @@
+
+from common.rolloutenv import *
+from common.perturbation import *
+from common.corrector import *
+from common.reinforce_corrector import *
+
+if __name__ == "__main__":
+    
+
+        # create a perturbation
+    perturbator = NormalJittering(0, 20)
+    # perturbator = None
+
+
+    # create a corrector
+    corrector = None
+    # corrector = LowPassCorrector(5)
+
+    # Initialize the environment
+    env = GodotEnv(convert_action_space=True)
+    
+    
+    u_sim = VITE_USim([0, 0])
+
+    corr = ReinforceCorrector(env, u_sim, perturbator, learn = True, log = True)
+    # corr.training_loop(Corrector)
+    corr.training_loop()
+
+    env.close()
